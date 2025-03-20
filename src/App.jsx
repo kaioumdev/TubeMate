a22bd205ba4c0ec41e3be13ee647e14ea50af544
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-21 01:45:48 +0600

    feat(toggleMenu): successfully work the toggleMenu functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Provider } from 'react-redux'
 import Body from './components/Body'
 import Header from './components/Header'
+import store from './utils/redux/store';
 
 function App() {
 
   return (
-    <>
-      <Header></Header>
-      <Body></Body>
-    </>
+    <Provider store={store}>
+      <div>
+        <Header></Header>
+        <Body></Body>
+      </div>
+    </Provider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleMenu } from '../utils/redux/appSlice';
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const toggleMenuHandler = () => {
+        dispatch(toggleMenu())
+    }
     return (
         <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
             <div className='flex col-span-1'>
-                <img className='h-8' src="https://static.vecteezy.com/system/resources/previews/021/190/402/non_2x/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg" alt="menu" />
-                <img className='h-8 mx-5' src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png" alt="youtube-logo" />
+                <img className='h-8 cursor-pointer' onClick={() => toggleMenuHandler()} src="https://static.vecteezy.com/system/resources/previews/021/190/402/non_2x/hamburger-menu-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg" alt="menu" />
+                <a href="/">
+                    <img className='h-8 mx-5' src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png" alt="youtube-logo" />
+                </a>
             </div>
             <div className='col-span-10 px-10 text-center'>
                 <input className='w-1/2 border border-gray-400 p-2 rounded-l-full' type="text" placeholder="Search" />
