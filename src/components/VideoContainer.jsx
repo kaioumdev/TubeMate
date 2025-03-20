ad9779802b2c8b78defb65bbaf7d6394f8ee58fb
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-21 05:38:50 +0600

    feat(videoCard): click videoCard to go the watch page

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from '../utils/contants'
 import VideoCard from './VideoCard'
+import { Link } from 'react-router-dom'
 
 const VideoContainer = () => {
     const [videos, setVideos] = useState([])
@@ -16,7 +17,11 @@ const VideoContainer = () => {
     return (
         <div className='flex flex-wrap'>
             {
-                videos.map(video => <VideoCard key={video.id} info={video}></VideoCard>)
+                videos.map(video => (
+                    <Link to={"/watch?v=" + video.id} key={video.id}>
+                        <VideoCard key={video.id} info={video}></VideoCard>
+                    </Link>
+                ))
             }
         </div>
     )
