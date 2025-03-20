78b98a17b69d6fe059511c7d7beeace381f503de
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-20 16:56:11 +0600

    feat(sidebar): updated sidebar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             </div>
             <div className='col-span-10 px-10 text-center'>
                 <input className='w-1/2 border border-gray-400 p-2 rounded-l-full' type="text" placeholder="Search" />
-                <button className='border border-gray-400 p-2 rounded-r-full'>Search</button>
+                <button className='border border-gray-400 px-5 py-2 bg-gray-100 rounded-r-full'>🔍</button>
             </div>
             <div className='col-span-1'>
                 <img className='h-8' src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" alt="User" />
