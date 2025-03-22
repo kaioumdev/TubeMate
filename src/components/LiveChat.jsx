13d6312b8663118c4a89ed4e66611c5b14ae84f5
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-23 05:22:45 +0600

    feat(liveChat): successfully show the liveChat message

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,24 +1,24 @@
 import React, { useEffect } from 'react'
 import ChatMessage from './ChatMessage'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addMessage } from '../utils/redux/chatSlice';
 
 const LiveChat = () => {
     const dispatch = useDispatch();
-
+    const chatMessage = useSelector((store) => store.chat.message)
     useEffect(() => {
-        const polling = setTimeout(() => {
+        const polling = setInterval(() => {
             dispatch(addMessage({ name: "Kaioum Islam", message: "This is my fevaroite react library" }))
-        }, 200)
+        }, 2000)
         return () => {
             clearTimeout(polling)
         }
     }, [])
     return (
         <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
-            <ChatMessage name="Abdul Kaiyum" message="This is hello react app"></ChatMessage>
-            <ChatMessage name="Abdul Kaiyum" message="This is hello react app"></ChatMessage>
-            <ChatMessage name="Abdul Kaiyum" message="This is hello react app"></ChatMessage>
+            {
+                chatMessage.map((message, index) => <ChatMessage key={index} name={message.name} message={message.message} />)
+            }
         </div>
     )
 }
