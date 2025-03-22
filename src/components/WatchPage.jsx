63df063b914e9b1f9632b3194df8c46e2934f4ef
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-22 21:51:52 +0600

    feat(comments): create new commentsContainer componnet

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -11,9 +11,11 @@ const WatchPage = () => {
         dispatch(closeMenu())
     }, [])
     return (
-        <div className='px-5'>
-            <iframe width="1200" height="600"
-                src={"https://www.youtube.com/embed/" + searchParams.get('v')} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div>
+            <div className='px-5'>
+                <iframe width="1200" height="600"
+                    src={"https://www.youtube.com/embed/" + searchParams.get('v')} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
         </div>
     )
 }
