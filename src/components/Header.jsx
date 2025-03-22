8494ee960852406d6a2842077ebcb968fd734df3
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-22 19:45:01 +0600

    feat(showSuggestions): successfully showSuggestions dynamically

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import { YOUTUBE_SEARCH_API } from '../utils/contants';
 const Header = () => {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchSuggestions, setSearchSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false)
 
     useEffect(() => {
 
@@ -35,19 +36,25 @@ const Header = () => {
             </div>
             <div className='col-span-10 px-10'>
                 <div>
-                    <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className='w-1/2 border border-gray-400 p-2 rounded-l-full' type="text" placeholder="Search" />
+                    <input value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onFocus={() => setShowSuggestions(true)}
+                        onBlur={() => setShowSuggestions(false)}
+                        className='w-1/2 border border-gray-400 p-2 rounded-l-full' type="text" placeholder="Search" />
                     <button className='border border-gray-400 px-5 py-2 bg-gray-100 rounded-r-full'>🔍</button>
                 </div>
-                <div className='fixed bg-white py-2 px-2 w-[43rem] shadow-lg rounded-lg border border-gray-100'>
-                    <ul>
-                        {
-                            searchSuggestions.map((suggestion) =>
-                                <li key={suggestion} className='p-2 px-3 shadow-sm hover:bg-gray-100 cursor-pointer'>🔍 {suggestion}</li>
+                {
+                    showSuggestions && <div className='fixed bg-white py-2 px-2 w-[43rem] shadow-lg rounded-lg border border-gray-100'>
+                        <ul>
+                            {
+                                searchSuggestions.map((suggestion) =>
+                                    <li key={suggestion} className='p-2 px-3 shadow-sm hover:bg-gray-100 cursor-pointer'>🔍 {suggestion}</li>
 
-                            )
-                        }
-                    </ul>
-                </div>
+                                )
+                            }
+                        </ul>
+                    </div>
+                }
             </div>
             <div className='col-span-1'>
                 <img className='h-8' src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" alt="User" />
