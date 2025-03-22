e59eb11c54312e5632c60db821a1677ccb6715aa
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-23 05:14:09 +0600

    make dispatch action to update addMessage reducer

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,15 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const ChatMessage = ({ name, message }) => {
 
-    useEffect(() => {
-        const polling = setTimeout(() => {
-
-        }, 200)
-        return () => {
-            clearTimeout(polling)
-        }
-    }, [])
     return (
         <div className='flex items-center'>
             <img className='h-8' src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" alt="User" />
--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatMessage from './ChatMessage'
+import { useDispatch } from 'react-redux';
+import { addMessage } from '../utils/redux/chatSlice';
 
 const LiveChat = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const polling = setTimeout(() => {
+            dispatch(addMessage({ name: "Kaioum Islam", message: "This is my fevaroite react library" }))
+        }, 200)
+        return () => {
+            clearTimeout(polling)
+        }
+    }, [])
     return (
         <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
             <ChatMessage name="Abdul Kaiyum" message="This is hello react app"></ChatMessage>
