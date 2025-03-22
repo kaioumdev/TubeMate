c9a3ec231c5dd84b9fed95a8304173fe76da769e
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-23 05:25:08 +0600

    feat(overflow): solve the UI issue to implements overflow-y-scroll

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -15,7 +15,7 @@ const LiveChat = () => {
         }
     }, [])
     return (
-        <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg'>
+        <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll'>
             {
                 chatMessage.map((message, index) => <ChatMessage key={index} name={message.name} message={message.message} />)
             }
