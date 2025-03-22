87e8dce6f1bfe021c194fba9aee5a47fe03de6cd
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-23 03:37:15 +0600

    using setTimeOut & cleare the clearTimeOut

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const ChatMessage = ({ name, message }) => {
+
+    useEffect(() => {
+        const polling = setTimeout(() => {
+
+        }, 200)
+        return () => {
+            clearTimeout(polling)
+        }
+    }, [])
     return (
         <div className='flex items-center'>
             <img className='h-8' src="https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png" alt="User" />
