9423f29ef37095b4447d966661af7e238a40911b
kaiyumdev <afrankaiyum34@gmail.com>
2025-03-23 12:00:55 +0600

    feat(clearInputField): after make new chat message to clear input text field

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -11,7 +11,7 @@ const LiveChat = () => {
     useEffect(() => {
         const polling = setInterval(() => {
             dispatch(addMessage({ name: generateRandomName(), message: makeRandomMessage(20) }))
-        }, 500)
+        }, 1500)
         return () => {
             clearTimeout(polling)
         }
@@ -29,6 +29,7 @@ const LiveChat = () => {
                 e.preventDefault()
                 console.log('submit', liveMessage)
                 dispatch(addMessage({ name: 'Kaioum Islam', message: liveMessage }))
+                setLiveMessage("")
             }} className='flex w-full p-2 border border-black'>
                 <input className='w-96 border border-black' type='text' value={liveMessage} onChange={(e) => setLiveMessage(e.target.value)} />
                 <button className='px-2 mx-2 bg-green-100'>Send</button>
