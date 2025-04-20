8f8a579de9f25ad2a984b43c48964fec9b2b4d57
kaiyumdev <afrankaiyum34@gmail.com>
2025-04-20 23:01:29 +0600

    replace frameBorder to solve the warnings

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -17,7 +17,7 @@ const WatchPage = () => {
             <div className='px-5 flex w-full'>
                 <div>
                     <iframe width="1200" height="600"
-                        src={"https://www.youtube.com/embed/" + searchParams.get('v')} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        src={"https://www.youtube.com/embed/" + searchParams.get('v')} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
                 <div className='w-full'>
                     <LiveChat></LiveChat>
