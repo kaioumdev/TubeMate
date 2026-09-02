791a2aac557aefe9a245f33889880eb42ee38c33
kaioumdev <afrankaiyum34@gmail.com>
2026-09-02 23:37:33 +0600

    feat implements button dynamically

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import Sidebar from './Sidebar'
-import MainContainer from './MainContainer'
 import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Sidebar from './Sidebar'
 
 const Body = () => {
-    return (
-        <div className='grid grid-flow-col'>
-            <Sidebar></Sidebar>
-            <Outlet></Outlet>
+  return (
+    <div className="app-container">
+      {/* Header is inside the router tree so useNavigate works */}
+      <Header />
+
+      <div className="app-body">
+        <Sidebar />
+        <div className="main-content">
+          <Outlet />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Body
