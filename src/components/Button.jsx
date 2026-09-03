8b89823e6e36943ac91b70cf56d84744a75682a9
kaioumdev <afrankaiyum34@gmail.com>
2026-09-03 23:47:48 +0600

    feat button implements

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Button = ({ name }) => {
-    return (
-        <div>
-            <button className='px-5 py-2 m-2 bg-gray-200 rounded-lg'>{name}</button>
-        </div>
-    )
+const Button = ({ name, active, onClick }) => {
+  return (
+    <button
+      className={`yt-chip${active ? ' active' : ''}`}
+      onClick={onClick}
+      aria-pressed={active}
+    >
+      {name}
+    </button>
+  )
 }
 
 export default Button
