9e7dda4f611dc91c62e3278d641ca581c06b7ae2
kaioumdev <afrankaiyum34@gmail.com>
2026-09-06 23:44:11 +0600

    complete the redux store configuration successfully

--- src/utils/redux/store.js
+++ src/utils/redux/store.js
@@ -1,14 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import appSlice from "./appSlice";
-import searchSlice from "./searchSlice";
-import chatSlice from "./chatSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import appSlice   from './appSlice'
+import searchSlice from './searchSlice'
+import chatSlice  from './chatSlice'
+import authSlice  from './authSlice'
 
 const store = configureStore({
-    reducer: {
-        app: appSlice,
-        search: searchSlice,
-        chat: chatSlice
-    }
+  reducer: {
+    app:    appSlice,
+    search: searchSlice,
+    chat:   chatSlice,
+    auth:   authSlice,
+  },
 })
 
-export default store;
+export default store
